6d9bc5c73043ce4f6ecbff94cdf82c04d1228da2
Maelle05 <maelle.rabouan@gmail.com>
2021-02-08 12:52:39 +0100

    Gyroscope fonctionel

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Gyroscope } from 'expo-sensors';
+
 
 export default function App() {
+  const [data, setData] = useState({
+    x: 0,
+    y: 0,
+    z: 0,
+  });
+
+  Gyroscope.addListener(gyroscopeData => {
+    setData(gyroscopeData);
+  })
+
+  const { x, y, z } = data;
+
+  // console.log('x: ' + Math.round(x)+' y: ' + Math.round(y) +' z: '+ Math.round(z))
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Zinzin Jump</Text>
+      <Text style={styles.text}>
+          x: {Math.round(x)} y: {Math.round(y)} z: {Math.round(z)}
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
