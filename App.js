605462fa82ffb915c2e2fa30c9a9110298b53236
Maelle05 <maelle.rabouan@gmail.com>
2021-02-09 17:07:10 +0100

    init

--- App.js
+++ App.js
@@ -3,29 +3,34 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Gyroscope } from 'expo-sensors';
 
-
 export default function App() {
+
+  // initialisation des 3 axes du Gyroscope
   const [data, setData] = useState({
     x: 0,
     y: 0,
     z: 0,
   });
 
-  Gyroscope.addListener(gyroscopeData => {
-    setData(gyroscopeData);
-  })
+  const [posPerso, setposPerso] = useState(0); // initialisation du state que gere la position du perso
 
   const { x, y, z } = data;
 
-  // console.log('x: ' + Math.round(x)+' y: ' + Math.round(y) +' z: '+ Math.round(z))
-
+  
+  Gyroscope.setUpdateInterval(16);
+  Gyroscope.addListener(gyroscopeData => { 
+    setData(gyroscopeData); 
+  });
+  
+  console.log('x: ' + Math.round(x)+' y: ' + Math.round(y) +' z: '+ Math.round(z))// Math.round => retourne la valeur d' Y au nombre arrondi à l'entier le plus proche
+ 
   return (
     <View style={styles.container}>
       <Text>Zinzin Jump</Text>
-      <Text style={styles.text}>
+      <Text style={styles.text , { transform: [{ translateX: posPerso }]}}>
           x: {Math.round(x)} y: {Math.round(y)} z: {Math.round(z)}
       </Text>
-      <StatusBar style="auto" />
+      <StatusBar style="auto"/>
     </View>
   );
 }
@@ -37,4 +42,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  box:{
+
+  }
 });
