9053704ae5df54720bdd7692ae8b895313275d39
Maelle05 <maelle.rabouan@gmail.com>
2021-02-10 20:21:17 +0100

    deplacement des paltformes au jumpjeux jouable MVP mais avec des bugs

--- myComponents/Game/Game.js
+++ myComponents/Game/Game.js
@@ -2,21 +2,25 @@ import { StatusBar } from 'expo-status-bar';
 import React, {useState, useEffect } from 'react';
 import { View, StyleSheet, Dimensions } from 'react-native';
 import { Accelerometer } from 'expo-sensors';
-import { GameEngine } from "react-native-game-engine"
 
 
-export default function Accele() {
+export default function Game() {
     // GLOBALE
     const screenWidth = Dimensions.get('screen').width
     const screenHeight = Dimensions.get('screen').height
-    const gravity = 3
-    const widthPlat = 100
-    const heightPlat = 20
-    const marginBottomPlat = [100, 200]
-    const marginLeftPlat = [50, 190]
+    const [gravity, setGravity] = useState(3)
+    const widthPlat = 70
+    const heightPlat = 15
+    const pousse = 7
+    const [marginBottomPlat, setmarginBottomPlat] = useState([100, 170, 220,300, 340, 450, 510, 20]) // Initialisation des coordonnées de toutes les platformes
+    const marginLeftPlat = [50, 190, 280, 10, 200, 300, 190, 70] // Initialisation des coordonnées de toutes les platformes
 
     function gameOver(){
         alert('You Lose !')
+    }
+
+    function dificulty() {
+        // setTimeout(function(){ setGravity(gravity + 1) }, 1000); // toute les secondes la gravité est de plus en plus forte 
     }
 
     // ACCELEROMETRE
@@ -33,13 +37,13 @@ export default function Accele() {
 
     // PLAYER
     const [jump, setJump] = useState(false)
-    const [bottomPlayer, setbottomPlayer] = useState(screenHeight / 2);
+    const [bottomPlayer, setbottomPlayer] = useState(screenHeight / 3);
 
-        // JUPM
+        // Jump
         function isJump(isjump) { //fonction isJump avec jump en paramétre 
             if(isjump){
-                setbottomPlayer(bottomPlayer => bottomPlayer + gravity+7) 
-                setTimeout(function(){ setJump(!jump) }, 900);
+                setbottomPlayer(bottomPlayer => bottomPlayer + gravity + pousse) 
+                setTimeout(function(){ setJump(!jump) }, 400); // Au bout de 900 ms jump re passe a false donc le player re-desend 
             }else{
                 setbottomPlayer(bottomPlayer => bottomPlayer - gravity)
             } 
@@ -49,24 +53,19 @@ export default function Accele() {
         useEffect(()=>{
             if(bottomPlayer > 0){
                 gameTimerPlayer = setInterval(()=>{
-                    // appelle de la fonction Jump
+                    // appelle de la fonction Jump / Gravity
                     isJump(jump)
                     // COllISION CHECK
-                    for(let i = 0; i < marginBottomPlat.length; i++){ // pour toute les platformes repertiorier dans le tableau marginBottomPlat
-                        if(bottomPlayer - 20 < marginBottomPlat[i] +2 && bottomPlayer - 20 > marginBottomPlat[i] - 2 
-                            && (175 + -x*200) > marginLeftPlat[i] && (175 + -x*200)< marginLeftPlat[i] + widthPlat ){
-                        setJump(!jump) // Si la collision est validé jump passe a true 
-                        }
-                    }
+                    collisionCheck()
+                    dificulty()
                     
-                },30)// Toutes les 30 ms 
+                },20)// Toutes les 30 ms 
                 return()=> {
                     clearInterval(gameTimerPlayer)
                 }
             }
             if(bottomPlayer < 0){
                 gameOver()
-                setbottomPlayer(screenHeight/2)
             }
         },[bottomPlayer]);
 
@@ -82,11 +81,33 @@ export default function Accele() {
             )
         }
 
+        // Mouv Platforme
+        function movePlat() {
+            for(let i = 0; i < marginBottomPlat.length; i++){
+                marginBottomPlat[i] = marginBottomPlat[i] - 100
+                if(marginBottomPlat[i] < 0){
+                    marginBottomPlat[i] = 500 + marginBottomPlat[i]
+                }
+            }
+        }
 
-        
+        // Collision check
+        function collisionCheck() {
+            for(let i = 0; i < marginBottomPlat.length; i++){ // pour toute les platformes repertiorier dans le tableau marginBottomPlat
+                if(bottomPlayer - 20 < marginBottomPlat[i] +2 && bottomPlayer - 20 > marginBottomPlat[i] - 2 
+                    && (175 + -x*200) > marginLeftPlat[i] && (175 + -x*200)< marginLeftPlat[i] + widthPlat ){
+                    setJump(!jump) // Si la collision est validé jump passe a true 
+                    movePlat() // Appelle de la fonction qui fait dessendre les platformes
+                }
+            }
+        }
+    
+
+
+    
     return (
         <View style={styles.gameContainer, { backgroundColor: 'grey', height: screenHeight - screenHeight/3, width: screenWidth, zIndex: 0, display: 'flex', flexDirection: 'column-reverse'}} >  
-            <View style={styles.player,{marginLeft : 175 + -x*200, marginBottom: bottomPlayer, position: 'absolute',  backgroundColor: 'pink', height: 20, width: 20, zIndex: 1}}> 
+            <View style={styles.player,{marginLeft : 175 + -x*200, marginBottom: bottomPlayer, position: 'absolute',  backgroundColor: 'pink', height: 20, width: 20}}> 
             </View>
             {platform}
             <StatusBar style="auto"/>
